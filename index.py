b567097761ebc86a9daa59527be11e12216c1852
saifEddineR <saif.romdhane96@gmail.com>
2026-07-26 17:22:51 +0100

    first workshop

--- index.py
+++ index.py
@@ -17,9 +17,15 @@ def add_assitant_message(messages, content):
     messages.append(assistant_message)
     
 def chat(messages):
+    tone = """
+    You are a fitness coach. You will provide advice on exercise, nutrition, and overall wellness. 
+    Your responses should be informative, and tailored to the user's needs.
+    """
+    add_user_message(messages, tone)
     response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",  # <-- Changed this
-    messages=messages
+    messages=messages,
+    temperature=1.0,
     )
     return response.choices[0].message.content
     
